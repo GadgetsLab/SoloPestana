f5772de33ce6ecd166cd2b634d36f0a000a77676
RDuuke <juuanduuke@gmail.com>
2016-10-07 18:07:06 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <title>SoloPestañas</title>
+    <link rel="stylesheet" href="css/ed-grid.css">
+    <link rel="stylesheet" href="css/template.css">
+</head>
+<body>
+<header id="header" class="bg-pink">
+    <div class="ed-container">
+        <div class="ed-item base-100 web-30 center-text">
+            <a href="index.php">
+                <img src="images/logo.png" alt="SoloPestañas" id="logo">
+            </a>
+        </div>
+        <div class="ed-item base-100 web-70">
+            <div class="ed-item base-100 derecha-texto">
+                <a href="">Facebook</a>
+                <a href="">Youtube</a>
+                <a href="">Instagram</a>
+            </div>
+            <div class="ed-item base-100">
+                <img src="images/barra_rosada.png" alt="">
+            </div>
+            <div class="ed-item base-100 desde-web">
+                <nav>
+                    <ul class="menu">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Nosotros</a>
+                        </li>
+                        <li>
+                            <a href="">Servicios</a>
+                        </li>
+                        <li>
+                            <a href="">Productos</a>
+                        </li>
+                        <li>
+                            <a href="">De interés</a>
+                        </li>
+                        <li>
+                            <a href="">Contacto</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<div id="slider">
+    <div class="ed-container no-padding">
+        <div class="ed-item base-100">
+            <img src="http://placehold.it/1200x404" alt="">
+        </div>
+    </div>
+</div>
+<div id="separator" class="bg-pink dark"></div>
+<div id="servicios" class="bg-img-total-fancy">
+    <div class="ed-container tablet-tabla bg-gray border-radius-20 servicios">
+        <div class="ed-item base-base-100 tablet-25">
+            <h3 class="text-purple center-text text-uppercase"><span class="circle">&nbsp;&nbsp;&nbsp;Look Premium&nbsp;</span></h3>
+            <img src="http://placehold.it/300x200" alt="">
+            <p class="text-justify bg-white padding-0-5-em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+        <div class="ed-item base-base-100 tablet-25">
+            <h3 class="text-purple center-text text-uppercase"><span class="circle dos">&nbsp;&nbsp;&nbsp;Look Seda&nbsp;</span></h3>
+            <img src="http://placehold.it/300x200" alt="">
+            <p class="text-justify bg-white padding-0-5-em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+        <div class="ed-item base-base-100 tablet-25">
+            <h3 class="text-purple center-text text-uppercase"><span class="circle tres">&nbsp;&nbsp;&nbsp;Look HD&nbsp;</span></h3>
+            <img src="http://placehold.it/300x200" alt="">
+            <p class="text-justify bg-white padding-0-5-em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+        <div class="ed-item base-base-100 tablet-25">
+            <h3 class="text-purple center-text no-before text-uppercase"><span class="circle cuatro">&nbsp;&nbsp;&nbsp;Remoción de extensiones&nbsp;</span></h3>
+            <img src="http://placehold.it/300x200" alt="">
+            <p class="text-justify bg-white padding-0-5-em">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="ed-container">
+        <div class="ed-item base-100 tablet-50 tablet-offset-25 center-text">
+            <div class="bg-img-fancy" style="padding: 2em;margin: -21px -31px 0px -23px;">
+                <div class="video">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/cYyr9cDn2nc" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h3 class="center-text text-uppercase text-purple light">-Testimonios-</h3>
+</div>
+<div id="testimonios">
+    <hr style="border-color: rgba(128,128,128,.2);">
+    <div class="ed-container tablet-tabla testimonios">
+        <div class="ed-item base-100 tablet-1-3">
+            <h4 class="text-purple light">Lina Marcela</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+        <div class="ed-item base-100 tablet-1-3">
+            <h4 class="text-purple light">Lina Marcela</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+        <div class="ed-item base-100 tablet-1-3 no-after">
+            <h4 class="text-purple light">Lina Marcela</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dolore,</p>
+        </div>
+    </div>
+    <hr style="border-color: rgba(128,128,128,.2);">
+</div>
+<div id="redesSociales">
+    <div class="ed-container">
+        <div class="ed-item base-100 tablet-40 tablet-offset-10">
+            <h4 class="center-text">Facebook</h4>
+            <a href="">Seguir</a>
+        </div>
+        <div class="ed-item base-100 tablet-40">
+            <h4 class="center-text">Instagram</h4>
+            <a href="">Seguir</a>
+        </div>
+    </div>
+</div>
+<div id="gmaps">
+    <h2 class="center-text">Mapa</h2>
+</div>
+<footer id="footer" class="bg-purple text-white">
+    <br>
+    <h3 class="center-text text-uppercase" style="font-size: 1.3em">-Contáctanos-</h3>
+    <div class="ed-container tablet-tabla center-text">
+        <div class="ed-item base-100 tablet-1-3">
+            <p>
+                Cra 66A # 34B - 5(Int. 201)(Barrio Conquistadores)<br>
+                Cra 74 # 48 - 37 Local 1017 (C.C Obelisco)<br>
+                Medellin - Colombia
+            </p>
+        </div>
+        <div class="ed-item base-100 tablet-1-3">
+            <p>
+                (+57)(4)574 78 54 - 583 87 63 <br>
+                (+57)300 462 24 48 <br>
+                (+57)301 281 77 42 WhatsApp
+            </p>
+        </div>
+        <div class="ed-item base-100 tablet-1-3">
+            info@solopestanas.com
+        </div>
+    </div>
+    <div class="ed-container full center-text bg-gray light text-purple">
+        <h5 class="center-text" style="width: 100%;margin: 1em 0">&copy;Copyright Solopestañas <?= date('Y') ?></h5>
+    </div>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/template.js.js"></script>
+</body>
+</html>
